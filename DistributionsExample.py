a93cc8e284abdc78fdea850d5549e6a35add7860
kyajmiller <zjm203@yahoo.com>
2015-09-10 14:39:30 -0700

    word lengths distribution example from lecture

---
+++ DistributionsExample.py
@@ -0,0 +1,21 @@
+__author__ = 'Kya'
+import nltk
+from nltk.book import *
+from pylab import *
+
+maxLength = 20
+lengths = arange(1, maxLength+1, 1)
+counts = [0] * maxLength
+wordLengths = [len(w) for w in text1]
+
+#count
+for len in wordLengths:
+    safeLen = len
+    if safeLen > maxLength:
+        safeLen = maxLength - 1
+    counts[safeLen-1] += 1
+
+#plot
+bar(lengths, counts)
+xlabel("Word Lengths")
+ylabel("Counts")
