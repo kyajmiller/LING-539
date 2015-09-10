8111a8a3be4217425dfed78d3dcf61746ad56807
kyajmiller <zjm203@yahoo.com>
2015-09-10 15:02:56 -0700

    doDiceEntropyManual allows you to manually input values to calculate the entropy

--- EntropyDice.py
+++ EntropyDice.py
@@ -20,7 +20,20 @@ def doDiceEntropy(diceSides):
     print("P_x: %s" % diceP_x)
     print("Entropy of %s sided dice: %s bits" % (diceSides, entropy(diceP_x)))
 
+
+def doDiceEntropyManual(diceP_x):
+    print("P_x: %s Entropy %s" % (diceP_x, entropy(diceP_x)))
+
 # main program
+'''
 doDiceEntropy(6)
 doDiceEntropy(4)
 doDiceEntropy(20)
+'''
+
+doDiceEntropyManual([0.25, 0.25, 0.25, 0.25])
+doDiceEntropyManual([0.4, 0.2, 0.2, 0.2])
+doDiceEntropyManual([0.55, 0.15, 0.15, 0.15])
+doDiceEntropyManual([0.7, 0.1, 0.1, 0.1])
+doDiceEntropyManual([0.85, 0.05, 0.05, 0.05])
+doDiceEntropyManual([0.97, 0.01, 0.01, 0.01])
