cab63ad030276a26db28f648df4f508ab5df5daa
kyajmiller <zjm203@yahoo.com>
2015-09-17 13:05:08 -0700

    refactor

--- Homework 1/q1b.py
+++ Homework 1/q1b.py
@@ -1,4 +1,11 @@
-__author__ = 'Kya'
+"""
+Kya Miller
+LING 539 Assignment 1
+Q1B - Reading in sentences from sents_in.txt, prints to console the probabilities of those sentences given the word
+probabilities from the browntag corpus
+"""
+
+from __future__ import division
 import q1a
 
 sentsInFile = open('sents_in.txt', 'r')
@@ -6,3 +13,9 @@ sentsInData = sentsInFile.readlines()
 sentsInData = [sentence.strip() for sentence in sentsInData]
 
 wordsFrequencySortedList = q1a.makeWordsSortedList()
+
+totalWords = 0
+for word in wordsFrequencySortedList:
+    totalWords += word[1]
+
+wordsProbabilities = [(word[0], word[1] / totalWords) for word in wordsFrequencySortedList]
