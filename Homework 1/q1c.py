9dfe5d7ee0c9fbadadc1c5efbbfc01d0e7daf63c
kyajmiller <zjm203@yahoo.com>
2015-09-22 21:56:07 -0700

    initial commit for this file, doesn't do much yet, will come back to it later

---
+++ Homework 1/q1c.py
@@ -0,0 +1,9 @@
+"""
+Kya Miller
+LING 539 Assignment 1
+Q1C - Plots the frequency of untagged words from the browntag corpus versus their word rank, then draws a line of best fit
+according to Zipf's Law.
+"""
+
+import q1a
+import matplotlib.pyplot as plt
