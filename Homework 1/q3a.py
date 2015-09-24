64a0757565005bf08d65fbeaa0e3334941916f50
kyajmiller <zjm203@yahoo.com>
2015-09-24 15:38:10 -0700

    commented out unneeded stuff

--- Homework 1/q3a.py
+++ Homework 1/q3a.py
@@ -2,8 +2,7 @@
 Kya Miller
 LING 539 Assignment 1
 Q3A - Calculates the entropy from the sum of the rolls of two 4-sided dice. Both dice are fairly weighted.
-I wasn't sure if the problem wanted the separate entropies of each dice added together, or the entropy of the probabilities
-of getting each possible sum from the two dice rolls (2-8), so I did both.
+Also includes the entropy of the separate die in a commented section.
 """
 
 from __future__ import division
@@ -18,10 +17,12 @@ def calculateEntropy(listOfProbabilities):
     return -sum
 
 
+'''
 dice1Entropy = calculateEntropy([0.25, 0.25, 0.25, 0.25])
 dice2Entropy = calculateEntropy([0.25, 0.25, 0.25, 0.25])
 sumEntropy = dice1Entropy + dice2Entropy
 print("The sum of the entropies of two fairly weighted dice: %s" % sumEntropy)
+'''
 
 totalSumsEntropy = calculateEntropy([1 / 16, 2 / 16, 3 / 16, 4 / 16, 3 / 16, 2 / 16, 1 / 16])
 print("The entropy of the sums of two fairly weighted dice: %s" % totalSumsEntropy)
