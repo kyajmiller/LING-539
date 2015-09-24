6312ca8b6f5a5550cfc061cdf0ca5587501277d5
kyajmiller <zjm203@yahoo.com>
2015-09-24 15:51:43 -0700

    get rankX and frequencyY lists

--- Homework 1/q1c.py
+++ Homework 1/q1c.py
@@ -7,3 +7,20 @@ according to Zipf's Law.
 
 import q1a
 import matplotlib.pyplot as plt
+
+'''
+-plot points by putting in the rank by the distribution
+xscale('log')
+yscale('log')
+pyplot.scatter
+makes line by .plot
+'''
+
+wordsFrequencySortedList = q1a.makeWordsSortedList()
+rankByFrequency = [[i + 1, wordsFrequencySortedList[i][1]] for i in range(len(wordsFrequencySortedList))]
+
+rankX = [rank for rank, frequency in rankByFrequency]
+frequencyY = [frequency for rank, frequency in rankByFrequency]
+
+print(rankX[:10])
+print(frequencyY[:10])
