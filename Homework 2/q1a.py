a4398b1f4461be54cdae61708be7e34e73752cf8
zjm203 <zjm203@email.arizona.edu>
2015-10-05 11:27:17 -0700

    added documentation strings. Documentation is cancer.

--- Homework 2/q1a.py
+++ Homework 2/q1a.py
@@ -11,22 +11,30 @@ from collections import Counter
 
 
 def makeTop100List(tokensList):
+    # uses Counter module to make a frequency dictionary. Takes list of words/pos tokens as argument. Returns list of
+    # top 100 most common tokens with their counts in descending order.
     frequencyDict = Counter()
     for token in tokensList:
         frequencyDict[token] += 1
 
     return frequencyDict.most_common(100)
 
+
+#open browntag_nolines.txt as input
 filein = open('browntag_nolines.txt', 'r')
 brownTagNoLines = filein.read()
 filein.close()
 
+#open ngram_frequencies.txt as output file
 outputFile = open('ngram_frequencies.txt', 'w')
 
+#tokenize input file on whitespace to get individual words_posTags. Declare separate words and posTags lists to be populated.
 wordsPOSTags = brownTagNoLines.split(' ')
 wordsUnigrams = []
 posTagsUnigrams = []
 
+# splits each word_posTag on underscore '_'. Sometimes there are multiple underscores in the token, so will redo the split
+#if find second underscore. Append split parts to wordsUnigrams and posTagsUnigrams as appropriate.
 for token in wordsPOSTags:
     splitWordPOS = token.split('_', 1)
     word = splitWordPOS[0]
@@ -38,6 +46,7 @@ for token in wordsPOSTags:
     wordsUnigrams.append(word)
     posTagsUnigrams.append(tag)
 
+#create bigrams and trigrams lists using string interpolation
 wordsBigrams = ['%s\t%s' % (wordsUnigrams[i], wordsUnigrams[i + 1]) for i in range(len(wordsUnigrams) - 1)]
 wordsTrigrams = ['%s\t%s\t%s' % (wordsUnigrams[i], wordsUnigrams[i + 1], wordsUnigrams[i + 2]) for i in
                  range(len(wordsUnigrams) - 2)]
@@ -46,6 +55,7 @@ posTagsBigrams = ['%s\t%s' % (posTagsUnigrams[i], posTagsUnigrams[i + 1]) for i 
 posTagsTrigrams = ['%s\t%s\t%s' % (posTagsUnigrams[i], posTagsUnigrams[i + 1], posTagsUnigrams[i + 2]) for i in
                    range(len(posTagsUnigrams) - 2)]
 
+#make top 100 lists using previously declared function
 wordsUnigramsTop100List = makeTop100List(wordsUnigrams)
 wordsBigramsTop100List = makeTop100List(wordsBigrams)
 wordsTrigramsTop100List = makeTop100List(wordsTrigrams)
@@ -54,6 +64,7 @@ posUnigramsTop100List = makeTop100List(posTagsUnigrams)
 posBigramsTop100List = makeTop100List(posTagsBigrams)
 posTrigramsTop100List = makeTop100List(posTagsTrigrams)
 
+#write results to output file with some formatting.
 outputFile.write('Top 100 Unigrams (words)')
 outputFile.write('\n--------\n')
 
