f27b983b5cdeebcb62d3b73d7fb93ab66878c557
zjm203 <zjm203@email.arizona.edu>
2015-10-05 13:15:16 -0700

    make unigrams and bigrams frequency lists

--- Homework 2/q1b.py
+++ Homework 2/q1b.py
@@ -5,3 +5,44 @@ Q1B - Reads in sentences from sents_in.txt and prints the probability of that se
 model where unknown words have probability 1.0; bigram model with Laplace smoothing; bigram model with Lidstone
 smoothing.
 """
+
+import re
+from collections import Counter
+
+
+def makeFrequencyList(tokensList):
+    # uses Counter module to make a frequency dictionary. Takes list of words/pos tokens as argument. Returns list of
+    # tokens with their counts in descending order.
+    frequencyDict = Counter()
+    for token in tokensList:
+        frequencyDict[token] += 1
+
+    return frequencyDict.most_common()
+
+
+# open browntag_nolines.txt as input
+filein = open('browntag_nolines.txt', 'r')
+brownTagNoLines = filein.read()
+filein.close()
+
+# tokenize input file on whitespace to get individual words_posTags. Declare words and list to be populated.
+wordsPOSTags = brownTagNoLines.split(' ')
+wordsUnigrams = []
+
+# splits each word_posTag on underscore '_'. Sometimes there are multiple underscores in the token, so will redo the
+# split if find second underscore. Append word part wordsUnigrams.
+for token in wordsPOSTags:
+    splitWordPOS = token.split('_', 1)
+    word = splitWordPOS[0]
+    tag = splitWordPOS[1]
+    if re.search('_', tag):
+        resplit = tag.split('_', 1)
+        word = '%s%s' % (word, resplit[0])
+        tag = resplit[1]
+    wordsUnigrams.append(word)
+
+# create bigrams list using string interpolation
+wordsBigrams = ['%s\t%s' % (wordsUnigrams[i], wordsUnigrams[i + 1]) for i in range(len(wordsUnigrams) - 1)]
+
+unigramsFrequencyList = makeFrequencyList(wordsUnigrams)
+bigramsFrequencyList = makeFrequencyList(wordsBigrams)
