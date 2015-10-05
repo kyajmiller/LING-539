71efa875b609ea97fcee918b019eb18465f36ce8
zjm203 <zjm203@email.arizona.edu>
2015-10-05 13:10:49 -0700

    initial commit

---
+++ Homework 2/q1b.py
@@ -0,0 +1,7 @@
+"""
+Kya Miller
+LING 539 Assignment 2
+Q1B - Reads in sentences from sents_in.txt and prints the probability of that sentence based on three models: unigram
+model where unknown words have probability 1.0; bigram model with Laplace smoothing; bigram model with Lidstone
+smoothing.
+"""
