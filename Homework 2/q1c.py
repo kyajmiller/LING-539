9c3cf35b2b08cc9fe0fea60c68e1d4393e653337
kyajmiller <zjm203@yahoo.com>
2015-10-10 13:53:32 -0700

    get bigrams list

--- Homework 2/q1c.py
+++ Homework 2/q1c.py
@@ -5,18 +5,66 @@ Q1C - Prints a log/log graph of Zipf's law for unsmoothed bigrams and smoothed b
 q1b.py. This imports both q1a.py and q1b.py.
 """
 
-from q1a import *
-from q1b import *
+import re
 import numpy
 import matplotlib.pyplot as plt
+from collections import Counter
+
+
+def getBigramsList():
+    # open browntag_nolines.txt as input
+    filein = open('browntag_nolines.txt', 'r')
+    brownTagLineByLine = [line.strip() for line in filein]
+    filein.close()
+
+    wordsUnigrams = []
+    wordsBigrams = []
+
+    # now read in line by line to get unigrams and sentence specific bigrams + trigrams
+    for line in brownTagLineByLine:
+        wordsPOSTags = line.split(' ')
+
+        # declare separate lists to generate bigrams and trigrams
+        lineWordsUnigramsToMakeBigrams = ['#']
+
+        # splits each word_posTag on underscore '_'. Sometimes there are multiple underscores in the token, so will redo the
+        # split if find second underscore.
+        for token in wordsPOSTags:
+            splitWordPOS = token.split('_', 1)
+            if len(splitWordPOS) == 2:
+                word = splitWordPOS[0]
+                tag = splitWordPOS[1]
+                if re.search('_', tag):
+                    resplit = tag.split('_', 1)
+                    word = '%s%s' % (word, resplit[0])
+                    tag = resplit[1]
+                wordsUnigrams.append(word)
+                lineWordsUnigramsToMakeBigrams.append(word)
+
+        # append appropriate number of # to bigrams and trigrams lists
+        lineWordsUnigramsToMakeBigrams.append('#')
+
+        # create bigrams and trigrams using string interpolation and append to appropriate lists
+        for i in range(len(lineWordsUnigramsToMakeBigrams) - 1):
+            wordsBigrams.append('%s\t%s' % (lineWordsUnigramsToMakeBigrams[i], lineWordsUnigramsToMakeBigrams[i + 1]))
+
+    return wordsBigrams
+
+
+def makeBigramsFrequencyList(wordsBigrams):
+    bigramsFrequencyDict = Counter()
+    for bigram in wordsBigrams:
+        bigramsFrequencyDict[bigram] += 1
+
+    return bigramsFrequencyDict.most_common()
 
 
 def plotUnsmoothedZipfsLogLog():
     rankX = [i + 1 for i in range(len(bigramsFrequencySortedList))]
-    frequencyY = [bigramsFrequencySortedList[i][1] for i in range(len(bigramsFrequencySortedList))]
+    frequencyYUnsmoothed = [bigramsFrequencySortedList[i][1] for i in range(len(bigramsFrequencySortedList))]
 
     logX = [numpy.log10(x) for x in rankX]
-    logY = [numpy.log10(y) for y in frequencyY]
+    logY = [numpy.log10(y) for y in frequencyYUnsmoothed]
 
     plt.figure()
     plt.scatter(logX, logY)
@@ -27,19 +75,43 @@ def plotUnsmoothedZipfsLogLog():
     plt.show()
 
 
-# get the bigrams count from q1a.py; reads in the browntag corpus, gets the bigrams, and uses the Counter module to
-# create a dictionary of the counts; uses makeFrequencyList function to return a list of the counts in descending order
-bigramsFrequencySortedList = makeFrequencyList(wordsBigrams)
+def plotLidstoneSmoothedZipfsLogLog():
+    rankX = [i + 1 for i in range(len(bigramsFrequencySortedList))]
+    frequencyYSmoothed = [bigramsFrequencySortedList[i][1] + alpha for i in range(len(bigramsFrequencySortedList))]
+
+    # add data for the first and last ranked unseen bigram
+    rankX.append(unseenBigramRankFirst)
+    rankX.append(unseenBigramsRankLast)
+    frequencyYSmoothed.append(unseenBigramFrequency)
+    frequencyYSmoothed.append(unseenBigramFrequency)
+
+    if len(rankX) == len(frequencyYSmoothed):
+        print('length matches')
+
+
+# get the bigrams; reads in the browntag corpus, gets the bigrams, and uses the Counter module to
+# create a dictionary of the counts; uses makeBigramsFrequencyList function to return a list of the counts in descending order
+bigramsFrequencySortedList = makeBigramsFrequencyList(getBigramsList())
+
+# get total seen bigrams count
+totalSeenBigrams = 0
+for bigram in bigramsFrequencySortedList:
+    totalSeenBigrams += bigram[1]
+
+# calculate total possible bigrams
+totalPossibleBigrams = totalSeenBigrams * totalSeenBigrams
 
 # calculate how many unseen bigrams there are
-unseenBigramsCount = totalPossibleBigrams - totalSeenBigrams
+totalUnseenBigrams = totalPossibleBigrams - totalSeenBigrams
 
 # each unseen bigram has a count of 0.65, the same as the alpha value from q1b.py Lidstone model.
-unseenBigramCount = 0.65
+alpha = 0.65
+unseenBigramFrequency = alpha
 
 # get the rank of the first and last unseen bigrams
 unseenBigramRankFirst = totalSeenBigrams + 1
 unseenBigramsRankLast = totalPossibleBigrams
+
 
 
 plotUnsmoothedZipfsLogLog()
