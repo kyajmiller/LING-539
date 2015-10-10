711e6243ba78d9d76e1652d4488b908f15f906b2
kyajmiller <zjm203@yahoo.com>
2015-10-10 13:29:39 -0700

    get unseen bigram data

--- Homework 2/q1c.py
+++ Homework 2/q1c.py
@@ -2,10 +2,11 @@
 Kya Miller
 LING 539 Assignment 2
 Q1C - Prints a log/log graph of Zipf's law for unsmoothed bigrams and smoothed bigrams using the Lidstone smoothign from
-q1b.py.
+q1b.py. This imports both q1a.py and q1b.py.
 """
 
 from q1a import *
+from q1b import *
 import numpy
 import matplotlib.pyplot as plt
 
@@ -26,6 +27,19 @@ def plotUnsmoothedZipfsLogLog():
     plt.show()
 
 
+# get the bigrams count from q1a.py; reads in the browntag corpus, gets the bigrams, and uses the Counter module to
+# create a dictionary of the counts; uses makeFrequencyList function to return a list of the counts in descending order
 bigramsFrequencySortedList = makeFrequencyList(wordsBigrams)
+
+# calculate how many unseen bigrams there are
+unseenBigramsCount = totalPossibleBigrams - totalSeenBigrams
+
+# each unseen bigram has a count of 0.65, the same as the alpha value from q1b.py Lidstone model.
+unseenBigramCount = 0.65
+
+# get the rank of the first and last unseen bigrams
+unseenBigramRankFirst = totalSeenBigrams + 1
+unseenBigramsRankLast = totalPossibleBigrams
+
 
 plotUnsmoothedZipfsLogLog()
