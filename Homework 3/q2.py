6fc77824aca56b3c842435305d7450ecce1ea54e
kyajmiller <zjm203@yahoo.com>
2015-10-26 06:30:02 -0700

    function to get all possible combinations of the words

--- Homework 3/q2.py
+++ Homework 3/q2.py
@@ -5,3 +5,21 @@ Q2 - Simulates a simple Hidden Markov Model for a 3 word, 3 POS tag language. {w
 Finds all possible 3 word sentences. Finds most probable tag sequence for each sentence.
 Saves output and emission/transmission probabilities to hmm_output.txt.
 """
+
+import itertools
+
+
+def getAllSentences():
+    # the words list needs to have w1 and w2 repeated after w3, or else itertools won't consider a combination where
+    # w1 or w2 can appear after w3
+    words = ['w1', 'w2', 'w3', 'w2', 'w1']
+    allSentencesObject = itertools.combinations_with_replacement(words, 3)
+
+    # however, because it still counts those repeated w1 and w2 as separate objects, you will get repeats. So filter
+    # out the repeats.
+    allSentencesFilteredList = []
+    for sentence in allSentencesObject:
+        if sentence not in allSentencesFilteredList:
+            allSentencesFilteredList.append(sentence)
+
+    return allSentencesFilteredList
