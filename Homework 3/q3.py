f8ea972aadbf2fc72537f88f66a64a518705cd57
kyajmiller <zjm203@yahoo.com>
2015-10-26 08:58:43 -0700

    initial commit, added docstring

---
+++ Homework 3/q3.py
@@ -0,0 +1,7 @@
+"""
+Kya Miller
+LING 539 Assignment 3
+Q3 - Finds percentage of words which have multiple POS tags in the browntag corpus. Then, given a training/testing split
+percentage, the program learns the most likely POS tag for a given word and then calculates the percentage of words and
+sentences correctly tagged in the testing set.
+"""
