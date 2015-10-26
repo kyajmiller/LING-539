862ead653dbb0ce06dbafc3fa44eda330bd4c168
kyajmiller <zjm203@yahoo.com>
2015-10-26 11:17:56 -0700

    comments

--- Homework 3/q2.py
+++ Homework 3/q2.py
@@ -23,6 +23,7 @@ import itertools
 
 
 def getAllSentencesGivenLength(length):
+    # get all possible sentences of a given length. Yeah.
     # the words list needs to have w1 and w2 repeated after w3, or else itertools won't consider a combination where
     # w1 or w2 can appear after w3
     words = ['w1', 'w2', 'w3', 'w2', 'w1']
@@ -52,9 +53,12 @@ def getAllPossiblePathsGivenLength(length):
 
 
 def calculateAllPathsTransmissionProbabilities(pathsList):
+    # because there's a finite number of paths, and I didn't want to have to calculate this multiple times, this is the
+    # function to get all of the transmisison probabilities for each path
     allPathTransmissionProbs = []
 
     for path in pathsList:
+        # check first step in path, apply start -> state prob
         transitionProb = 1
 
         if path[0] == 't1':
@@ -64,6 +68,7 @@ def calculateAllPathsTransmissionProbabilities(pathsList):
         else:
             transitionProb *= 0.2
 
+        # change state
         for i in range(len(path) - 1):
             currentState = path[i]
             currentStateIndex = transitionsMappingDictionary[currentState]
@@ -78,18 +83,24 @@ def calculateAllPathsTransmissionProbabilities(pathsList):
 
 
 def calculateBestPathForSentenceGivenLength(length):
+    # calculates the best path for a sentence using transmission/emission probabilities
     allSentences = getAllSentencesGivenLength(length)
     allPaths = getAllPossiblePathsGivenLength(length)
 
+    # this is for pretty printing later, don't worry about it
     sentenceStrings = [' '.join(sentence) for sentence in allSentences]
     pathStrings = [' '.join(path) for path in allPaths]
 
+    # yay transmission probs from previous function
     allPathTransmissionProbs = calculateAllPathsTransmissionProbabilities(allPaths)
 
+    # loop through sentences
     for i in range(len(allSentences)):
         sentence = allSentences[i]
         sentenceString = sentenceStrings[i]
         pathProbs = []
+
+        # check all the paths
         for i in range(len(allPaths)):
             path = allPaths[i]
             pathTransmissionProb = allPathTransmissionProbs[i]
@@ -108,10 +119,12 @@ def calculateBestPathForSentenceGivenLength(length):
             pathProb = pathTransmissionProb * pathEmissionsProb
             pathProbs.append(pathProb)
 
+        # get index of highest probability path
         bestProbIndex = pathProbs.index(max(pathProbs))
         bestProbPathString = pathStrings[bestProbIndex]
         bestProb = pathProbs[bestProbIndex]
 
+        # different print formats for different length things, I suck at doing dynamic formatting.
         if length == 1:
             formattedString = '%s\t\t\t\t%s\t\t\t\t\t\t%s\n' % (sentenceString, bestProbPathString, bestProb)
         elif length == 2:
@@ -120,8 +133,11 @@ def calculateBestPathForSentenceGivenLength(length):
             formattedString = '%s\t\t%s\t\t\t\t%s\n' % (sentenceString, bestProbPathString, bestProb)
         outputFile.write(formattedString)
 
+
+# open output file hmm_output.txt
 outputFile = open('hmm_output.txt', 'w')
 
+# declare transitions matrix and emissions matrix
 transitionsMatrix = [
     [0.2, 0.6, 0.2],
     [0.3, 0.3, 0.4],
@@ -134,12 +150,14 @@ emissionsMatrix = [
     [0.1, 0.7, 0.2]
 ]
 
+# dictionaries for matrix indices
 transitionsMappingDictionary = {'t1': 0, 't2': 1, 't3': 2}
 emissionsMappingDictionary = {'t1': 0, 't2': 1, 't3': 2, 'w1': 0, 'w2': 1, 'w3': 2}
 
 beginningTableString = 'Sentence\t\tWinningTagSequence\t\tProduct\n'
 outputFile.write(beginningTableString)
 
+# do the things
 calculateBestPathForSentenceGivenLength(1)
 calculateBestPathForSentenceGivenLength(2)
 calculateBestPathForSentenceGivenLength(3)
