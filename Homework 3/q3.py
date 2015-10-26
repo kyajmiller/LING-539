45ea2b5d6fc02523e8c5a28d1a0ec3bb43fc03ff
kyajmiller <zjm203@yahoo.com>
2015-10-26 09:57:15 -0700

    beginning of function to get most common POS tag per word in the training set, far from complete, copy of previous code

--- Homework 3/q3.py
+++ Homework 3/q3.py
@@ -8,6 +8,7 @@ sentences correctly tagged in the testing set.
 
 from __future__ import division
 import re
+import math
 from collections import Counter
 
 
@@ -65,6 +66,68 @@ def getPercentageOfWordsWithMoreThanOnePOSTag():
 
     percentWordsMoreThanOnePOS = (wordsMoreThanOnePOS / len(wordsDict)) * 100
     return percentWordsMoreThanOnePOS
+
+
+def trainAndTestNaiveTagger(trainingPercentage):
+    trainingPercentageInteger = trainingPercentage * 100
+    testingPercentage = 1 - trainingPercentage
+
+    # calculate how many lines should be in training and testing
+    numTotalLines = len(brownTagLineByLine)
+    numTrainingLines = math.ceil((numTotalLines * trainingPercentage))
+    numTestingLines = numTotalLines - numTrainingLines
+
+    # use slicing to get the training and testing sets
+    trainingSet = brownTagLineByLine[:numTrainingLines]
+    testingSet = brownTagLineByLine[-numTestingLines:]
+
+
+def getMostCommonPOSTagPerWord(trainingSet):
+    # first just read in browntag stuff and get unique words and unigrams in a list
+    wordsDict = Counter()
+    posDict = Counter()
+
+    # get unique words and unigrams, establish words dictionary
+    for line in brownTagLineByLine:
+        wordsPOSTags = line.split(' ')
+
+        for token in wordsPOSTags:
+            splitWordPOS = token.split('_', 1)
+            if len(splitWordPOS) == 2:
+                word = splitWordPOS[0]
+                tag = splitWordPOS[1]
+                if re.search('_', tag):
+                    resplit = tag.split('_', 1)
+                    word = '%s%s' % (word, resplit[0])
+                    tag = resplit[1]
+                word = word.lower()
+                wordsDict[word] += 1
+                posDict[tag] += 1
+
+    # now that we have the dictionary of words, clear out the entries to make room for lists of POS tags
+    # each entry has two lists, first list is pos tags, second list is count
+    wordsPOSTagsDict = {word: [[], []] for word in wordsDict}
+
+    # then cycle back through the browntag stuff and populate the dictionary with lists of pos tags
+    for line in brownTagLineByLine:
+        wordsPOSTags = line.split(' ')
+
+        for token in wordsPOSTags:
+            splitWordPOS = token.split('_', 1)
+            if len(splitWordPOS) == 2:
+                word = splitWordPOS[0]
+                tag = splitWordPOS[1]
+                if re.search('_', tag):
+                    resplit = tag.split('_', 1)
+                    word = '%s%s' % (word, resplit[0])
+                    tag = resplit[1]
+                word = word.lower()
+                if tag not in wordsPOSTagsDict[word][0]:
+                    wordsPOSTagsDict[word][0].append(tag)
+                    wordsPOSTagsDict[word][1].append(1)
+                else:
+                    tagIndex = wordsPOSTagsDict[word][0].index(tag)
+                    wordsPOSTagsDict[word][1][tagIndex] += 1
 
 # open browntag_nolines.txt as input
 filein = open('browntag_nolines.txt', 'r')
