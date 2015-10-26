89cb6f3f54e255b367f8d92b439d3b70d713637c
kyajmiller <zjm203@yahoo.com>
2015-10-26 09:21:19 -0700

    should make a dictionary of both words and frequency and words with list of pos tags

--- Homework 3/q3.py
+++ Homework 3/q3.py
@@ -6,6 +6,7 @@ percentage, the program learns the most likely POS tag for a given word and then
 sentences correctly tagged in the testing set.
 """
 
+from __future__ import division
 import re
 from collections import Counter
 
@@ -14,11 +15,13 @@ filein = open('browntag_nolines.txt', 'r')
 brownTagLineByLine = [line.strip() for line in filein]
 filein.close()
 
+# first just read in browntag stuff and get unique words and unigrams in a list
 wordsUnigrams = []
 posTagsUnigrams = []
 
-wordsNumTagsDict = Counter()
+wordFrequencyDict = Counter()
 
+# get unique words and unigrams, establish words dictionary
 for line in brownTagLineByLine:
     wordsPOSTags = line.split(' ')
 
@@ -32,7 +35,39 @@ for line in brownTagLineByLine:
                 word = '%s%s' % (word, resplit[0])
                 word = word.lower()
                 tag = resplit[1]
+            wordFrequencyDict[word] += 1
             if word not in wordsUnigrams:
                 wordsUnigrams.append(word)
             if tag not in posTagsUnigrams:
                 posTagsUnigrams.append(tag)
+
+# now that we have the dictionary of words, clear out the entries to make room for lists of POS tags
+wordsPOSTagsDict = wordFrequencyDict[:]
+for word in wordsPOSTagsDict:
+    wordsPOSTagsDict[word] = []
+
+# then cycle back through the browntag stuff and populate the dictionary with lists of pos tags
+for line in brownTagLineByLine:
+    wordsPOSTags = line.split(' ')
+
+    for token in wordsPOSTags:
+        splitWordPOS = token.split('_', 1)
+        if len(splitWordPOS) == 2:
+            word = splitWordPOS[0]
+            tag = splitWordPOS[1]
+            if re.search('_', tag):
+                resplit = tag.split('_', 1)
+                word = '%s%s' % (word, resplit[0])
+                word = word.lower()
+                tag = resplit[1]
+            if tag not in wordFrequencyDict[word]:
+                wordsPOSTagsDict[word].append(tag)
+
+wordsMoreThanOnePOS = 0
+for word in wordsPOSTagsDict:
+    if len(wordsPOSTagsDict[word]) > 1:
+        wordsMoreThanOnePOS += 1
+
+percentWordsMoreThanOnePOS = (wordsMoreThanOnePOS / len(wordsUnigrams)) * 100
+print(len(wordsUnigrams))
+print(wordsMoreThanOnePOS)
