962ccb1d204f6bae0931799461452c111e46b8f2
kyajmiller <zjm203@yahoo.com>
2015-10-26 06:07:46 -0700

    nothing yet, just docstring

---
+++ Homework 3/q2.py
@@ -0,0 +1,7 @@
+"""
+Kya Miller
+LING 539 Assignment 3
+Q2 - Simulates a simple Hidden Markov Model for a 3 word, 3 POS tag language. {w1, w2, w3}, {tag1, tag2, tag3}
+Finds all possible 3 word sentences. Finds most probable tag sequence for each sentence.
+Saves output and emission/transmission probabilities to hmm_output.txt.
+"""
