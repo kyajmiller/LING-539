1d9dd8c9113ed4e1a065952ce2c50dc88228f57c
kyajmiller <zjm203@yahoo.com>
2015-11-08 13:19:19 -0700

    reformatted doc string

--- Homework 4/q1.py
+++ Homework 4/q1.py
@@ -14,21 +14,13 @@ starting probabilities matrix
         t1      t2      t3      t4      t5
 start   0.15    0.25    0.20    0.25    0.15
 
-transitions matrix
-    t1      t2      t3      t4      t5
-t1  0.2     0.2     0.2     0.2     0.2
-t2  0.1     0.15    0.5     0.15    0.1
-t3  0.3     0.1     0.3     0.1     0.2
-t4  0.4     0.05    0.1     0.4     0.05
-t5  0.2     0.3     0.1     0.1     0.3
-
-emissions matrix
-    w1      w2      w3      w4
-t1  0.25    0.25    0.25    0.25
-t2  0.3     0.2     0.3     0.2
-t3  0.3     0.1     0.3     0.3
-t4  0.1     0.2     0.3     0.4
-t5  0.2     0.3     0.2     0.3
+transitions matrix                              emissions matrix
+    t1      t2      t3      t4      t5              w1      w2      w3      w4
+t1  0.2     0.2     0.2     0.2     0.2         t1  0.25    0.25    0.25    0.25
+t2  0.1     0.15    0.5     0.15    0.1         t2  0.3     0.2     0.3     0.2
+t3  0.3     0.1     0.3     0.1     0.2         t3  0.3     0.1     0.3     0.3
+t4  0.4     0.05    0.1     0.4     0.05        t4  0.1     0.2     0.3     0.4
+t5  0.2     0.3     0.1     0.1     0.3         t5  0.2     0.3     0.2     0.3
 """
 from __future__ import division
 import math
