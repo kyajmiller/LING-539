c124b3e82ff2c3ef1a37055a0028ddbed471191f
kyajmiller <zjm203@yahoo.com>
2015-11-08 13:36:37 -0700

    gets untagged treebank sentences

--- Homework 4/q2.py
+++ Homework 4/q2.py
@@ -1,1 +1,10 @@
-__author__ = 'Kya'
+"""
+Kya Miller
+LING 539 Assignment 4
+Q2 - Imports a portion of the Penn Treebank as available through nltk and uses the first 90% as training data and the
+remaining 10% as testing data. Tests the performance of various nltk taggers using the training and testing data.
+"""
+
+from nltk.corpus import treebank
+
+treebankSentences = treebank.sents()
