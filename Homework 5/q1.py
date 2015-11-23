c64776ec8fb0313ea456845cdf5774385f27e60a
kyajmiller <zjm203@yahoo.com>
2015-11-22 17:02:34 -0700

    open and read in input files, open output file

---
+++ Homework 5/q1.py
@@ -0,0 +1,19 @@
+"""
+Kya Miller
+LING 539 Assignment 5
+Q1 - Reads in sents_source.txt and sents_target.txt as input files, prints the number of words in each sentence to
+output.txt. Calculates sentence alignment between sents_source and sents_target, and prints the output to console.
+"""
+
+# read in sents_source.txt, store sentences in array
+sourceSentencesFileIn = open('sents_source.txt', 'r')
+sourceSentences = [line.strip() for line in sourceSentencesFileIn.readlines()]
+sourceSentencesFileIn.close()
+
+# read in sents_target.txt, store sentences in array
+targetSentencesFileIn = open('sents_target.txt', 'r')
+targetSentences = [line.strip() for line in targetSentencesFileIn.readlines()]
+targetSentencesFileIn.close()
+
+# open output file
+outputFile = open('output.txt', 'w')
