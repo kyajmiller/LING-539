4e699cefb537da2ec00fc1a1ad2e414615085cd5
kyajmiller <zjm203@yahoo.com>
2015-11-22 17:09:02 -0700

    add beginning of function to do WordCounts for source sentences

--- Homework 5/q1.py
+++ Homework 5/q1.py
@@ -10,6 +10,12 @@ def getSentenceWordCount(sentence):
     words = sentence.split(' ')
     return len(words)
 
+
+def doWordCountsForSourceSentencesAndPrintToOutput():
+    for i in range(len(sourceSentences)):
+        numWords = getSentenceWordCount(sourceSentences[i])
+
+
 # read in sents_source.txt, store sentences in array
 sourceSentencesFileIn = open('sents_source.txt', 'r')
 sourceSentences = [line.strip() for line in sourceSentencesFileIn.readlines()]
@@ -22,3 +28,5 @@ targetSentencesFileIn.close()
 
 # open output file
 outputFile = open('output.txt', 'w')
+headerString = 'Sentence\tNumberOfWords'
+outputFile.write(headerString)
