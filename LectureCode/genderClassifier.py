824897a62b9d4629ab32cdac2be952bcef559149
kyajmiller <zjm203@yahoo.com>
2015-12-01 14:42:52 -0700

    add function gendered features

--- LectureCode/genderClassifier.py
+++ LectureCode/genderClassifier.py
@@ -7,3 +7,13 @@ Kya --> Female, Peter --> Male
 import nltk
 from nltk.corpus import names
 import random
+
+
+def gender_features(name):
+    features = {}
+    features['lastLetter'] = name[-1]
+    return features
+
+
+features = gender_features('peter')
+print features
