9007cc91520d7af8ad966d911315a5ba4c94e577
kyajmiller <zjm203@yahoo.com>
2015-12-05 18:41:44 -0700

    read in gold label file, get first 3000 labels

--- Final Project/q1.py
+++ Final Project/q1.py
@@ -3,3 +3,13 @@ Kya Miller
 LING 539 Assignment 6 - Final Project
 Q1 -
 """
+
+goldDataFileIn = open('goldData/index.txt', 'r')
+goldDataLines = goldDataFileIn.readlines()
+goldDataFileIn.close()
+
+goldDataLabels = []
+for line in goldDataLines[:3000]:
+    splitLine = line.split(' ', 1)
+    label = splitLine[0]
+    goldDataLabels.append(label)
