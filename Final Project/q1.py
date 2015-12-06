700aea282bea765cd2c95da7f9f0e4168a347f97
kyajmiller <zjm203@yahoo.com>
2015-12-05 18:49:04 -0700

    convert gold labels retrieval to function

--- Final Project/q1.py
+++ Final Project/q1.py
@@ -4,16 +4,20 @@ LING 539 Assignment 6 - Final Project
 Q1 -
 """
 
-goldDataFileIn = open('goldData/index.txt', 'r')
-goldDataLines = goldDataFileIn.readlines()
-goldDataFileIn.close()
 
-goldDataLabels = []
-for line in goldDataLines[:3000]:
-    splitLine = line.split(' ', 1)
-    label = splitLine[0]
-    goldDataLabels.append(label)
+def getGoldLabels():
+    goldDataFileIn = open('goldData/index.txt', 'r')
+    goldDataLines = goldDataFileIn.readlines()
+    goldDataFileIn.close()
 
-trainingGoldLabels = goldDataLabels[:1000]
-developmentGoldLabels = goldDataLabels[1000:2000]
-testingGoldLabels = goldDataLabels[2000:3000]
+    goldDataLabels = []
+    for line in goldDataLines[:3000]:
+        splitLine = line.split(' ', 1)
+        label = splitLine[0]
+        goldDataLabels.append(label)
+
+    trainingGoldLabels = goldDataLabels[:1000]
+    developmentGoldLabels = goldDataLabels[1000:2000]
+    testingGoldLabels = goldDataLabels[2000:3000]
+
+    return trainingGoldLabels, developmentGoldLabels, testingGoldLabels
