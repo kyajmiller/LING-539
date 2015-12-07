9fcc6235260ddf2e596fd8ded45d958b44054357
zjm203 <zjm203@email.arizona.edu>
2015-12-07 09:51:09 -0700

    remove the unigrams and stopwords list functions, decreased accuracy

--- Final Project/q1.py
+++ Final Project/q1.py
@@ -127,64 +127,6 @@ def checkForMoney(emailText):
     return moneyCounter
 
 
-def getStopWordsList():
-    # this list of stopwords is what I typically use for NLP related stuff at work, so for the sake of convenience
-    # I just copied it over to use here
-    stopWordsList = ['a', 'about', 'above', 'across', 'after', 'again', 'against', 'all', 'almost', 'alone',
-                     'along', 'already', 'also', 'although', 'always', 'among', 'an', 'and', 'another', 'any',
-                     'anybody', 'anyone', 'anything', 'anywhere', 'are', 'area', 'areas', 'around', 'as', 'ask',
-                     'asked', 'asking', 'asks', 'at', 'away', 'back', 'backed', 'backing', 'backs', 'be', 'became',
-                     'because', 'become', 'becomes', 'been', 'before', 'began', 'behind', 'being', 'beings', 'best',
-                     'better', 'between', 'big', 'both', 'but', 'by', 'came', 'can', 'cannot', 'case', 'cases',
-                     'certain', 'certainly', 'clear', 'clearly', 'come', 'could', 'did', 'differ', 'different',
-                     'differently', 'do', 'does', 'done', 'down', 'downed', 'downing', 'downs', 'during', 'each',
-                     'early', 'either', 'end', 'ended', 'ends', 'enough', 'even', 'evenly', 'ever', 'every',
-                     'everybody', 'everyone', 'everything', 'everywhere', 'face', 'faces', 'fact', 'facts', 'far',
-                     'felt', 'find', 'finds', 'first', 'for', 'four', 'from', 'full', 'fully', 'further',
-                     'furthered', 'furthering', 'furthers', 'gave', 'general', 'generally', 'get', 'gets', 'give',
-                     'given', 'gives', 'go', 'going', 'good', 'goods', 'got', 'great', 'greater', 'greatest',
-                     'group', 'grouped', 'grouping', 'groups', 'had', 'has', 'have', 'having', 'he', 'her', 'here',
-                     'herself', 'high', 'higher', 'highest', 'him', 'himself', 'his', 'how', 'however', 'i', 'if',
-                     'important', 'in', 'interest', 'interested', 'interesting', 'interests', 'into', 'is', 'it',
-                     'its', 'itself', 'just', 'keep', 'keeps', 'kind', 'knew', 'know', 'known', 'knows', 'large',
-                     'largely', 'last', 'later', 'latest', 'least', 'less', 'let', 'lets', 'like', 'likely', 'long',
-                     'longer', 'longest', 'made', 'make', 'making', 'man', 'many', 'may', 'me', 'men', 'might',
-                     'more', 'most', 'mostly', 'mr', 'mrs', 'much', 'must', 'my', 'myself', 'necessary', 'need',
-                     'needed', 'needing', 'needs', 'never', 'new', 'newer', 'newest', 'next', 'no', 'nobody', 'non',
-                     'noone', 'not', 'nothing', 'now', 'nowhere', 'number', 'numbers', 'of', 'off', 'often', 'old',
-                     'older', 'oldest', 'on', 'once', 'one', 'only', 'open', 'opened', 'opening', 'opens', 'or',
-                     'order', 'ordered', 'ordering', 'orders', 'other', 'others', 'our', 'out', 'over', 'part',
-                     'parted', 'parting', 'parts', 'per', 'perhaps', 'place', 'places', 'point', 'pointed',
-                     'points', 'possible', 'present', 'presented', 'presenting', 'presents', 'problem', 'problems',
-                     'put', 'puts', 'quite', 'rather', 'really', 'right', 'room', 'rooms', 'said', 'same', 'saw',
-                     'say', 'says', 'second', 'seconds', 'see', 'seem', 'seemed', 'seeming', 'sees', 'several',
-                     'shall', 'she', 'should', 'show', 'showed', 'showing', 'shows', 'side', 'sides', 'since',
-                     'small', 'smaller', 'smallest', 'so', 'some', 'somebody', 'someone', 'something', 'somewhere',
-                     'state', 'states', 'still', 'such', 'sure', 'take', 'taken', 'than', 'that', 'the', 'their',
-                     'them', 'there', 'therefore', 'these', 'they', 'thing', 'things', 'think', 'thinks', 'this',
-                     'those', 'though', 'thought', 'thoughts', 'three', 'through', 'thus', 'to', 'today',
-                     'together', 'too', 'toward', 'turn', 'turned', 'turning', 'turns', 'two', 'u', 'under',
-                     'until', 'up', 'us', 'use', 'used', 'uses', 'very', 'want', 'wanted', 'wanting', 'wants',
-                     'was', 'way', 'ways', 'we', 'well', 'wells', 'went', 'were', 'what', 'when', 'where',
-                     'whether', 'which', 'while', 'who', 'whole', 'whose', 'why', 'will', 'with', 'within',
-                     'without', 'work', 'worked', 'working', 'works', 'would', 'year', 'years', 'yet', 'you',
-                     'young', 'younger', 'youngest', 'you', 'yours', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-                     'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2',
-                     '3', '4', '5', '6', '7', '8', '9', '0']
-
-    return stopWordsList
-
-
-def getUnigrams(emailText):
-    unigrams = []
-    unfilteredUnigrams = re.findall(r"[\w']+", emailText.lower())
-    for word in unfilteredUnigrams:
-        stopWords = getStopWordsList()
-        if word not in stopWords:
-            unigrams.append(word)
-    return unigrams
-
-
 def createFeatureSet(emailText):
     features = {}
     features['HTML'] = doesHTMLExist(emailText)
